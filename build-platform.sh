01de90c6fabafa5be0941a1f258f53e72d91c789
satyashani <satyashani@gmail.com>
2018-03-13 13:09:46 +0530

    aws cli

--- build-platform.sh
+++ build-platform.sh
@@ -5,4 +5,4 @@ wget -qO - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"
 sudo apt-get update
-sudo apt-get install -y openresty
+sudo apt-get install -y openresty awscli
