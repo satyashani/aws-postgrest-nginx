f34dfb22cf02eb0b099af98543918e2ff70ae869
satyashani <satyashani@gmail.com>
2018-03-13 10:29:13 +0530

    initing platform

--- build-platform.sh
+++ build-platform.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+
 wget -qO - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
 
-sudo apt-get install -y install software-properties-common
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"
 sudo apt-get update
 sudo apt-get install -y openresty
-cp nginx.conf /usr/local/openresty/nginx/conf/
-mkdir /usr/local/openresty/nginx/logs
