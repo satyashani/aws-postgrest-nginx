7abf74dcb105438f8d1600a05da9412499ec97c6
satyashani <satyashani@gmail.com>
2018-03-15 14:23:39 +0530

    update in heath check

--- builder/platform-uploads/opt/elasticbeanstalk/hooks/appdeploy/enact/06-setup-enhanced-health.sh
+++ builder/platform-uploads/opt/elasticbeanstalk/hooks/appdeploy/enact/06-setup-enhanced-health.sh
@@ -17,6 +17,14 @@ then
     /opt/elasticbeanstalk/bin/healthd-track-pidfile --proxy nginx
     /opt/elasticbeanstalk/bin/healthd-configure --appstat-log-path /usr/local/openresty/nginx/logs/access.log --appstat-uni sec --appstat-timestamp-on 'completion' 
 
+    RESTART_HEALTHD=''
+    ## Track application pids
+    PID_FILE="/usr/local/openresty/nginx/logs/nginx.pid"
+    if [ -f "$PID_FILE" ]; then
+        /opt/elasticbeanstalk/bin/healthd-track-pidfile --name nginx --location $PID_FILE 
+        RESTART_HEALTHD='true'
+    fi
+
     ## restart healthd 
     if [ ! -z "$RESTART_HEALTHD" ]; then
         /opt/elasticbeanstalk/bin/healthd-restart
